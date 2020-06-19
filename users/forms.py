8d157e6e86b5b3514f6b209d5d7b7f61b069f757
Varsha359 <gajulavarsha359@gmail.com>
2020-06-19 19:55:06 +0530

    Made few modifications

--- users/forms.py
+++ users/forms.py
@@ -5,13 +5,12 @@ from django.contrib.auth.forms import UserCreationForm
 class UserRegisterForm(UserCreationForm):
     email=forms.EmailField()
     phone= forms.CharField(max_length=10,required=True)
-    employee_id = forms.CharField(max_length=30, required=True)
+    hod_id = forms.CharField(max_length=30, required=True)
     designation = forms.CharField(max_length=30, required=True)
-    number_of_leaves= forms.CharField(max_length=2,required=True)
     department_number= forms.CharField(required=True)
     address = forms.CharField(max_length=200, required=True)
   
 
     class Meta:
         model=User
-        fields=['employee_id','password1','password2','username','department_number','designation','email','phone','address','number_of_leaves']
+        fields=['hod_id','password1','password2','username','department_number','designation','email','phone','address']
